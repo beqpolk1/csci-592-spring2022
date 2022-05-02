bc5f42e8fa78af1f1da472c46dd61e3a09537eb7
Ben P <polk.ben@gmail.com>
2022-05-02 00:35:10 -0600

    Use host.docker.internal address

--- project/DataConverter/src/DataConverter.java
+++ project/DataConverter/src/DataConverter.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public class DataConverter {
     public static void main(String[] args) {
-        MongoClient mongoClient = new MongoClient("192.168.1.10", 27017);
+        MongoClient mongoClient = new MongoClient("host.docker.internal", 27017);
 
         MongoDatabase mongoSongsDB = mongoClient.getDatabase("mongoSongs");
 
