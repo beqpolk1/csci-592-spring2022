cf41a39d29a62379d183f837942b26a0bd248e2d
Ben P <polk.ben@gmail.com>
2022-05-05 23:22:21 -0600

    Finish postgres create script

--- project/postgreSQLScripts/create_tables.sql
+++ project/postgreSQLScripts/create_tables.sql
@@ -18,11 +18,68 @@ CREATE TABLE track(
 DROP TABLE IF EXISTS track_genre;
 CREATE TABLE track_genre(
     track_fk UUID NOT NULL,
-    genre VARCHAR(100) NOT NULL
+    genre VARCHAR(100) NOT NULL,
+    FOREIGN KEY(track_fk) REFERENCES track(id) ON DELETE CASCADE
 );
 
 DROP TABLE IF EXISTS track_artist;
 CREATE TABLE track_artist(
     track_fk UUID NOT NULL,
-    artist_fk UUID NOT NULL
+    artist_fk UUID NOT NULL,
+    PRIMARY KEY(track_fk, artist_fk),
+    FOREIGN KEY(track_fk) REFERENCES track(id) ON DELETE NO ACTION,
+    FOREIGN KEY(artist_fk) REFERENCES artist(id) ON DELETE NO ACTION
+);
+
+DROP TABLE IF EXISTS album;
+CREATE TABLE album(
+    id UUID PRIMARY KEY NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    popularity INTEGER NOT NULL,
+    release_year INTEGER NOT NULL
+);
+
+DROP TABLE IF EXISTS artist_album;
+CREATE TABLE artist_album(
+    artist_fk UUID NOT NULL,
+    album_fk UUID NOT NULL,
+    PRIMARY KEY(artist_fk, album_fk),
+    FOREIGN KEY(artist_fk) REFERENCES artist(id) ON DELETE NO ACTION,
+    FOREIGN KEY(album_fk) REFERENCES album(id) ON DELETE NO ACTION
+);
+
+DROP TABLE IF EXISTS album_track;
+CREATE TABLE album_track(
+    album_fk UUID NOT NULL,
+    track_fk UUID NOT NULL,
+    track_number INTEGER,
+    PRIMARY KEY(album_fk, track_fk),
+    FOREIGN KEY(album_fk) REFERENCES album(id) ON DELETE NO ACTION,
+    FOREIGN KEY(track_fk) REFERENCES track(id) ON DELETE NO ACTION
+);
+
+DROP TABLE IF EXISTS album_format;
+CREATE TABLE album_format(
+    album_fk UUID NOT NULL,
+    format VARCHAR(100) NOT NULL,
+    FOREIGN KEY(album_fk) REFERENCES album(id) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS album_genre;
+CREATE TABLE album_genre(
+    album_fk UUID NOT NULL,
+    genre VARCHAR(100),
+    FOREIGN KEY(album_fk) REFERENCES album(id) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS review;
+CREATE TABLE review(
+    album_fk UUID NOT NULL,
+    journalist VARCHAR(100) NOT NULL,
+    rank VARCHAR(100) NOT NULL,
+    stars INTEGER NOT NULL,
+    medium_name VARCHAR(100),
+    medium_type VARCHAR(100),
+    medium_url VARCHAR(100),
+    FOREIGN KEY(album_fk) REFERENCES album(id) ON DELETE CASCADE
 );
